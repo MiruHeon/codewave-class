03cd6cd9f94d9e5ab95b9ae3fe41ddabd5363a63
YOUSOL <imjamminii@gmail.com>
2025-10-05 22:59:39 +0900

    test.c

--- test.c
+++ test.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LinkTree 10
+
 typedef struct Tree {
     int root;
     int node2;
     int node3;
-    struct Tree* leaf1;
-    struct Tree* leaf2;
+    struct Tree* link[LinkTree];
+    int link_count;
 } Tree;
 
-void LinkTree_leaf1(Tree* tree1, Tree* tree2) {
-    tree1->leaf1 = tree2;
-}
+void GDS(Tree* node) {
+    if (!node) return;
 
-void LinkTree_leaf2(Tree* tree1, Tree* tree2) {
-    tree1->leaf2 = tree2;
+    printf("노드 탐색: %d %d %d\n", node->root, node->node2, node->node3);
+
+    for (int i = 0; i < node->link_count; i++) {
+        GDS(node->link[i]);
+    }
 }
 
 int main(void) {
     Tree* tree1 = (Tree*)malloc(sizeof(Tree));
     Tree* tree2 = (Tree*)malloc(sizeof(Tree));
+    Tree* tree3 = (Tree*)malloc(sizeof(Tree));
+    Tree* tree4 = (Tree*)malloc(sizeof(Tree));
 
-    tree1->root = 40;
-    tree1->node2 = 50;
-    tree1->node3 = 60;
+    tree1->root = 1;   tree1->node2 = 35;  tree1->node3 = 32; tree1->link_count = 2;
+    tree2->root = 21;  tree2->node2 = 23;  tree2->node3 = 53; tree2->link_count = 1;
+    tree3->root = 3;   tree3->node2 = 8;   tree3->node3 = 9;  tree3->link_count = 0;
+    tree4->root = 6;   tree4->node2 = 153; tree4->node3 = 45; tree4->link_count = 0;
 
-    tree2->root = 10;
-    tree2->node2 = 20;
-    tree2->node3 = 30;
+    tree1->link[0] = tree2;
+    tree1->link[1] = tree3;
+    tree2->link[0] = tree4;
 
-    LinkTree_leaf1(tree1, tree2);
-
-    printf("Tree1 Root node1: %d\n", tree1->root);
-    printf("Tree1 leaf1 -> Tree2 node1: %d\n", tree1->leaf1->root);
+    printf("GDS 탐색 결과:\n");
+    GDS(tree1);
 
     free(tree1);
     free(tree2);
+    free(tree3);
+    free(tree4);
 
     return 0;
 }
+
