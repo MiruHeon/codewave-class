48ef6ec00e753589f60da870f8d8fe1cdff04be2
YOUSOL <imjamminii@gmail.com>
2025-10-05 22:27:35 +0900

    test.c

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 typedef struct Tree {
-    int node1;
+    int root;
     int node2;
     int node3;
     struct Tree* leaf1;
@@ -21,18 +21,18 @@ int main(void) {
     Tree* tree1 = (Tree*)malloc(sizeof(Tree));
     Tree* tree2 = (Tree*)malloc(sizeof(Tree));
 
-    tree1->node1 = 40;
+    tree1->root = 40;
     tree1->node2 = 50;
     tree1->node3 = 60;
 
-    tree2->node1 = 10;
+    tree2->root = 10;
     tree2->node2 = 20;
     tree2->node3 = 30;
 
     LinkTree_leaf1(tree1, tree2);
 
-    printf("Tree1 Root node1: %d\n", tree1->node1);
-    printf("Tree1 leaf1 -> Tree2 node1: %d\n", tree1->leaf1->node1);
+    printf("Tree1 Root node1: %d\n", tree1->root);
+    printf("Tree1 leaf1 -> Tree2 node1: %d\n", tree1->leaf1->root);
 
     free(tree1);
     free(tree2);
